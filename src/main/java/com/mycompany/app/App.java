6ee004855b8230766c0bb90c6e1a2688f23feaf9
artidarty <jesusguerrero.flores@gmail.com>
2022-02-23 18:48:01 -0600

    java-code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,38 +1,37 @@
 package com.mycompany.app;
 
-// Import ArrayList class inside of the java.util package
-import java.util.ArrayList;
-
-// Import all classes inside of java.security package
-import java.security.*;
-
-import java.util.Scanner;
-
-/**
- * Hello world!
- *
- */
-public class App 
+class App 
 {
-    public static void main( String[] args )
-    {
-        // System.out.println( "Hello World!" );
-        // // String Concatenation:
-        // System.out.println(
-        //     "Integer: " + 10 +  // integer
-        //     " Double: " + 3.14 + // decimal or float
-        //     " Boolean: " + true // tre or false
-        // );
-
-        System.out.print("Hello ");
-        System.out.print("World\n");
-        System.out.println("World");
-        // new line -> \n
-        // this.println("World");
-
-        System.out.println(Math.PI);
-        System.out.printf("pi2 = %.3f\n", Math.PI);
-
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+       App.myMeth(1);
+       int c = App.sum(6, 2);
+       System.out.println(c);
+       int d = (int) App.multi(3, 4);
+       System.out.println(d);
+       int e = App.operation(3, 3);
+       System.out.println(e);
     }
+
+    static void myMeth(int counter) {
+        for (int i = 0; i < counter; i++) {
+            System.out.println("hello world");
+        }       
+    }
+        
+    private static int sum(int a, int b) {
+        return a + b;
+    }
+    
+    public static double multi(int a, int b) {
+        return a * b;
+    }
+
+    private static int operation(int a, int b) {
+        if(a==b){
+            return a - b;
+        }
+        return a * b;
+       
+    }
+    
 }
